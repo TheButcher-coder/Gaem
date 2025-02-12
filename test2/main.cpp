608cec995a93cef3a07f40e5bc1f9411174ff452
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-02-12 13:08:04 +0100

    Gay ball follows you

--- test2/main.cpp
+++ test2/main.cpp
@@ -18,43 +18,44 @@ struct pos {
   }
 };
 
+
 int main()
 {
   // create the window
   sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+  pos oldpos;
 
   // run the program as long as the window is open
   while (window.isOpen())
   {
     // clear the window with black color
     window.clear(sf::Color::White);
-
     // draw everything here...
     sf::CircleShape shape(50.f);
 
 
 
     shape.setFillColor(sf::Color(100, 250, 50));
-    pos oldpos;
+    sf::CircleShape shape2(50.f);
+    shape2.setFillColor(sf::Color(100, 100, 100));
     // check all the window's events that were triggered since the last iteration of the loop
     while (const std::optional event = window.pollEvent())
     {
-      sf::CircleShape shape2(50.f);
-      shape2.setFillColor(sf::Color(100, 100, 100));
+
       if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>())
       {
         std::cout << "x: " << mouseMoved->position.x;
         std::cout << "\ty: " << mouseMoved->position.y << std::endl;
         shape2.setPosition(sf::Vector2f(mouseMoved->position.x, mouseMoved->position.y));
         oldpos.x = mouseMoved->position.x;
-        oldpos.y =mouseMoved->position.y;
+        oldpos.y = mouseMoved->position.y;
       }
-      else {
-        shape2.setPosition(sf::Vector2f(oldpos.x, oldpos.y));
-      }
-      window.draw(shape2);
-    }
 
+
+
+    }
+    shape2.setPosition(sf::Vector2f(oldpos.x, oldpos.y));
+    window.draw(shape2);
 
     //window.draw(shape2);
     // set the shape color to green
