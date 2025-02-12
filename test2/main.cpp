8647f28ccc16307683e96ae9b2efbf3b1d85ce35
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-02-12 13:11:26 +0100

    Cleanup/Comment

--- test2/main.cpp
+++ test2/main.cpp
@@ -24,46 +24,28 @@ int main()
   // create the window
   sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
   pos oldpos;
+  float r = 50;
 
   // run the program as long as the window is open
   while (window.isOpen())
   {
-    // clear the window with black color
     window.clear(sf::Color::White);
-    // draw everything here...
-    sf::CircleShape shape(50.f);
-
-
+    sf::CircleShape shape(r);
 
     shape.setFillColor(sf::Color(100, 250, 50));
-    sf::CircleShape shape2(50.f);
+    sf::CircleShape shape2(r);
     shape2.setFillColor(sf::Color(100, 100, 100));
     // check all the window's events that were triggered since the last iteration of the loop
     while (const std::optional event = window.pollEvent())
     {
-
-      if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>())
-      {
-        std::cout << "x: " << mouseMoved->position.x;
-        std::cout << "\ty: " << mouseMoved->position.y << std::endl;
-        shape2.setPosition(sf::Vector2f(mouseMoved->position.x, mouseMoved->position.y));
+      if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>()) {
+        //update mousepos
         oldpos.x = mouseMoved->position.x;
         oldpos.y = mouseMoved->position.y;
       }
-
-
-
     }
     shape2.setPosition(sf::Vector2f(oldpos.x, oldpos.y));
     window.draw(shape2);
-
-    //window.draw(shape2);
-    // set the shape color to green
-
-
-    //window.draw(shape);
-
-    // end the current frame
 
 
     window.display();
