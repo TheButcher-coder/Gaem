88e3b2c86c30682a9acc07cd040ab4310a8337e2
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-02-13 13:37:45 +0100

    Changed ball color to black

--- test2/main.cpp
+++ test2/main.cpp
@@ -24,7 +24,7 @@ int main()
   // create the window
   sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
   pos oldpos;
-  float r = 100;
+  float r = 50;
 
   // run the program as long as the window is open
   while (window.isOpen())
@@ -34,7 +34,7 @@ int main()
 
     shape.setFillColor(sf::Color(255, 250, 50));
     sf::CircleShape shape2(r);
-    shape2.setFillColor(sf::Color(255, 255, 255));
+    shape2.setFillColor(sf::Color(0, 0, 0));
     // check all the window's events that were triggered since the last iteration of the loop
     while (const std::optional event = window.pollEvent())
     {
@@ -46,7 +46,6 @@ int main()
     }
     shape2.setPosition(sf::Vector2f(oldpos.x - r, oldpos.y - r));
     window.draw(shape2);
-
 
     window.display();
   }
