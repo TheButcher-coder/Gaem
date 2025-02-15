89400861ddc8485f5f7dac95f17143f36188ecb2
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-02-15 18:38:54 +0100

    fixed, wtf its the same whatever

--- pong/main.cpp
+++ pong/main.cpp
@@ -33,7 +33,7 @@ int get_millisec() {
 void pong() {
     //Init all stuff thats needed
     int r = 10;
-    sf::RenderWindow win(sf::VideoMode({400, 400}), "Pong");
+    sf::RenderWindow win(sf::VideoMode({800, 400}), "Pong");
 
     sf::RectangleShape p1(sf::Vector2f(100, 20));   //Player 1
     sf::RectangleShape p2(sf::Vector2f(100, 20));   //Player 2
@@ -41,91 +41,19 @@ void pong() {
 
     p1.setPosition(sf::Vector2f(10, 10));
     p2.setPosition(sf::Vector2f(390, 10));
-    ball.setPosition(sf::Vector2f(200, 200));
+    ball.setPosition(sf::Vector2f(400, 200));
 
     //Basic window stuff
     while (win.isOpen()) {
         //TEST
         win.clear(sf::Color::Black);
-
-        p1.setFillColor(sf::Color::White);
-        p2.setFillColor(sf::Color::White);
-        ball.setFillColor(sf::Color(100, 100, 100));
-
-        win.draw(p1);
-        win.draw(p2);
         win.draw(ball);
-        //if (win.pollEvent()->getIf<sf::Event::Closed>()) win.close();   //close window if close button pressed
+        if (win.pollEvent()->getIf<sf::Event::Closed>()) win.close();   //close window if close button pressed
         win.display();
     }
 }
 
-void airhockeything() {
-    //window setup
-    sf::RenderWindow win(sf::VideoMode({800, 600}), "airhoggey");
-    srand(time(NULL));     //init rnaodm
 
-    int r = 25, n_balls = 10;
-    double t, t0=get_millisec();
-    std::vector<std::unique_ptr<sf::CircleShape>> balls;    //Ball vektor
-    std::vector<std::unique_ptr<Pos>> v_balls;
-    //place 10 random Balls
-    for (int i = 0; i < n_balls; i++) {
-        //Random initial pos
-        balls.emplace_back(std::make_unique<sf::CircleShape>(sf::CircleShape(r)));
-        balls[i]->setPosition(sf::Vector2f(rand()%(800-r), rand()%(600-r)));
-        //init speeds
-        v_balls.emplace_back(std::make_unique<Pos>(Pos(1, 1)));
-    }
-    v_balls[0]->set(1, 1);      //test der bewegung
-
-
-    while (win.isOpen()) {
-        double t1 = get_millisec();
-        double t = (t1 - t0) / 1000.0; // In Sekunden umwandeln
-        if (t > 1.0) t = 0.016; // Falls `t` zu groß wird, begrenzen (16ms ≈ 60 FPS)
-        t0 = t1; // t0 aktualisieren
-
-
-        //window stuff
-        win.clear(sf::Color::White);
-        if (win.pollEvent()->getIf<sf::Event::Closed>()) win.close();   //close window
-
-        for (int i = 0; i < n_balls; i++) {
-                    //Move balls
-                    balls[i]->setPosition(balls[i]->getPosition() + (v_balls[i]->get_v2f())*static_cast<float>(t));
-                }
-
-        /*
-        //Check colissions
-        for (int i=0; i < n_balls; i++) {
-            for (int j=i+1; j < n_balls; j++) {
-                if (balls[i]->getGlobalBounds().findIntersection(balls[j]->getGlobalBounds()).has_value()) {
-                    //COLLISION AHEAD!
-                    *v_balls[i] = (*v_balls[i] - *v_balls[j]);     //Define - for pos
-                    *v_balls[j] = (*v_balls[j] - *v_balls[i]);
-                }
-            }
-        }
-        */
-
-
-
-        for (auto& ball:balls) {
-            //render each ball
-            ball->setFillColor(sf::Color::Red);     //place balls
-            win.draw(*ball);
-        }
-        win.display();
-        t0 = t;
-        /*
-        for (auto& ball:balls) {
-            Pos(ball->getPosition()).print();
-        }
-        */
-        std::cout << "ZEIT: " << t << std::endl;
-    }
-}
 int main() {
     std::cout << "POOPER" << std::endl;
     pong();
