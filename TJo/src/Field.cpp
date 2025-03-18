8aa40c39b10715cfc4c728a62decf1f9cc5147f5
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-03-18 13:25:31 +0100

    stuff

--- TJo/src/Field.cpp
+++ TJo/src/Field.cpp
@@ -4,12 +4,21 @@
 
 #include "Field.h"
 
-Field::Field() {
-//init playing field
+Field::Field(shared_ptr<Card_shuffler> cs) {
+    //init playing field by drawing random cards
+    //Global card shuffler required
+  cards = vector<vector<Card>>(3, vector<Card>(4));
+
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 4; j++) {
+        cards[i][j] = cs->draw_card();
+      }
+    }
 }
 
 void Field::printField() {
 //print field
+    //FFS mag niiiiicht XD
 }
 
 void Field::update() {
--- TJo/src/Field.h
+++ TJo/src/Field.h
@@ -6,20 +6,25 @@
 #define FIELD_H
 
 #include "Card.h"
+#include "Card_shuffler.h"
 #include <vector>
-
+#include <memory>
+#include <iostream>
 
 using namespace std;
 
 
 class Field {
 private:
-    vector<vector<Card> > cards;
+    vector<vector<shared_ptr<Card>> > cards;
+    //shared_ptr<Card_shuffler> cs;    //enthält DEN Card shuffler Es kann nur einen geben, sonst ist die verteilung schlecht
 public:
-    Field();
+    Field(shared_ptr<Card_shuffler>);
     ~Field() {};
 
     void printField();
+    shared_ptr<Card> get_card(int i, char j);
+    void set_card(shared_ptr<Card> in);
     void update();
 };
 
