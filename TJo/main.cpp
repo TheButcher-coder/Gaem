4c309d801348fc5ad7bbea7d5ab814254ac753b1
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-03-18 12:19:35 +0100

    added Cardshuffler and test in main

--- TJo/main.cpp
+++ TJo/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
+
+#include "src/Card.h"
+#include "src/Card_shuffler.h"
 //Du Figgo
 
 using namespace std;
 
 
 int main() {
-
-    vector<vector<int>> test(3, vector<int>(3));
-    return 0;
+    Card_shuffler cs;
+    for (int i = 0; i < 20; i++) {
+        cout << cs.draw_card() << endl;
+    }
 }
