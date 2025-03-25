a760542071f1828c298b3d2eeada274e4487d5bb
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-03-25 12:48:39 +0100

    Init

---
+++ TJo/src/used_stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jakub on 25.03.25.
+//
+
+#include "used_stack.h"
---
+++ TJo/src/used_stack.h
@@ -0,0 +1,16 @@
+//
+// Created by Jakub on 25.03.25.
+//
+
+#ifndef USED_STACK_H
+#define USED_STACK_H
+
+
+
+class used_stack {
+
+};
+
+
+
+#endif //USED_STACK_H
