523eb0927051979587ce0c7c1ba7bd8506d3c3a4
Jakub Fleischer <jakub.fleischer03@gmail.com>
2025-10-21 18:29:34 +0200

    added shit broke shit

--- test_3d/main.cpp
+++ test_3d/main.cpp
@@ -6,7 +6,46 @@
 
 using namespace std;
 
-namespace linalg {//Vllt namespace mit matrix
+
+
+struct Point {
+protected:
+    vector<int> p;
+public:
+    Point() {
+        p = {0, 0, 0};
+    }
+    Point(int xin, int yin, int zin) {
+        p = {xin, yin, zin};
+    }
+    explicit Point(vector<int> &pin) :p(std::move(pin)) {}
+
+
+    vector<int> getp() {
+        return p;
+    }
+
+    void setp(vector<int> &pin) {
+        p = pin;
+    }
+    Point operator=(Point &p) {
+        //Point temp(p);
+        return p;
+    }
+};
+
+struct orientation_3d: private Point {
+private:
+    vector<double> rot;
+public:
+    orientation_3d() {
+        p = {0, 0, 0};
+    }
+};
+
+
+namespace linalg {
+    //Vllt namespace mit matrix
     struct Matrix {
     private:
         vector<vector<double>> val;
@@ -19,11 +58,15 @@ namespace linalg {//Vllt namespace mit matrix
             ny = nyin;
             val = vector<vector<double>> (nx, vector<double>(ny));
         }
-        Matrix(Matrix &min) {
+        Matrix(Matrix& min) {
             val = min.get_arr();
             nx = val.size();
             ny = val[0].size();
         }
+
+        Matrix(Matrix&& other) noexcept
+        : val(std::move(other.val)), nx(other.nx), ny(other.ny) {}
+
         explicit Matrix(const vector<vector<double>> &in) {
             val = in;
             nx = val.size();
@@ -50,6 +93,20 @@ namespace linalg {//Vllt namespace mit matrix
             return val;
         }
 
+        Matrix T() {
+            Matrix temp(ny, nx);
+            for (int i = 0; i < nx; i++) {
+                for (int j = 0; j < ny; j++) {
+                    temp.set(j, i, val[i][j]);
+                }
+            }
+            return temp;
+        }
+        /*
+                Matrix inv() {
+
+                }*/
+
         Matrix& operator=(Matrix in) {
             //swap(size, in.size);
             val.swap(in.get_arr());
@@ -57,8 +114,8 @@ namespace linalg {//Vllt namespace mit matrix
         }
 
         Matrix operator*(Matrix M2) {
-
             //Optimize to recursive algo -> faster better stronger!
+
             auto m1=val, m2=M2.get_arr();
             int r1=m1.size(), c1=m1[0].size(), r2=m2.size(), c2=m2[0].size();
             Matrix out(r1, c2);
@@ -75,58 +132,38 @@ namespace linalg {//Vllt namespace mit matrix
             }
             return out;
         }
+        Point operator*(Point pin) {
+            auto temp = pin.getp();
+            vector<int> res(3);
+            for (int i = 0; i < 3; i++) {
+                res[i] = val[i][0]*temp[0] + val[i][1]*temp[1] + val[i][2]*temp[2];
+            }
+            //Point out(res);
+            return Point(res);
+        }
 
     };
 
-    Matrix get_rx(double alpha) {
-        vector<vector<double>> temp = {{1, 0, 0, 0},{0, cos(alpha), -sin(alpha), 0}, {0, sin(alpha), cos(alpha), 0}, {0, 0, 0, 1}};
-        return Matrix(temp);
-    }
-    Matrix get_ry(double alpha) {
-        vector<vector<double>> temp = {{cos(alpha), 0, -sin(alpha), 0}, {0, 1, 0, 0}, {sin(alpha), 0, cos(alpha), 0}, {0, 0, 0, 1}};
-        return Matrix(temp);
-    }
-    Matrix get_rz(double alpha) {
-        vector<vector<double>> temp = {{cos(alpha), -sin(alpha), 0, 0},{sin(alpha),  cos(alpha), 0, 0}, {0, 0, 1, 0},  {0, 0, 0, 1}};
-        return Matrix(temp);
-    }
-    Matrix get_homo_trans(double x, double y, double z, double rx, double ry, double rz) {
-        Matrix temp(vector<vector<double>>({{1, 0, 0, x}, {0, 1, 0, y}, {0, 0, 1, z}, {0, 0, 0, 1}}));
-        return temp * get_rx(rx) * get_ry(ry) * get_rz(rz);
-    }
-}
+        Matrix get_rx(double alpha) {
+            vector<vector<double>> temp = {{1, 0, 0, 0},{0, cos(alpha), -sin(alpha), 0}, {0, sin(alpha), cos(alpha), 0}, {0, 0, 0, 1}};
+            return Matrix(temp);
+        }
+        Matrix get_ry(double alpha) {
+            vector<vector<double>> temp = {{cos(alpha), 0, -sin(alpha), 0}, {0, 1, 0, 0}, {sin(alpha), 0, cos(alpha), 0}, {0, 0, 0, 1}};
+            return Matrix(temp);
+        }
+        Matrix get_rz(double alpha) {
+            vector<vector<double>> temp = {{cos(alpha), -sin(alpha), 0, 0},{sin(alpha),  cos(alpha), 0, 0}, {0, 0, 1, 0},  {0, 0, 0, 1}};
+            return Matrix(temp);
+        }
 
-struct Point {
-protected:
-    vector<int> p;
-public:
-    Point() {
-        p = {0, 0, 0};
-    }
-    Point(int xin, int yin, int zin) {
-        p = {xin, yin, zin};
-    }
-    vector<int> getp() {
-        return p;
-    }
+        Matrix get_homo_trans(double x, double y, double z, double rx, double ry, double rz) {
+            Matrix temp(vector<vector<double>>({{1, 0, 0, x}, {0, 1, 0, y}, {0, 0, 1, z}, {0, 0, 0, 1}}));
+            return temp * get_rx(rx) * get_ry(ry) * get_rz(rz);
+        }
 
-    void setp(vector<int> &pin) {
-        p = pin;
-    }
-    Point operator=(Point &p) {
-        //Point temp(p);
-        return p;
-    }
 };
 
-struct orientation_3d: private Point {
-private:
-    vector<double> rot;
-public:
-    orientation_3d() {
-        p = {0, 0, 0};
-    }
-};
 struct Tri_3d {
 private:
     Point p1, p2, p3;
@@ -135,6 +172,57 @@ public:
         p1 = pin1;
         p2 = pin2;
         p3 = pin3;
+    }
+    Point at(int i) {
+        if (i == 0) return p1;
+        else if (i == 1) return p2;
+        else if (i == 2) return p3;
+        else exit(-690);
+    }
+    Point P1() {
+        return p1;
+    }
+    Point P2() {
+        return p2;
+    }
+    Point P3() {
+        return p3;
+    }
+
+};
+struct Cam {
+private:
+    linalg::Matrix trans;
+    int sx, sy;     //window size in pixels (0, 0, 0) is in middle of screen/view centre
+    string mode;    //"perspective" or "parallel"
+public:
+    Cam(): trans(4, 4) {
+        trans = linalg::Matrix(vector<vector<double> >({{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}));
+        sx = 600;
+        sy = 600;
+        mode = "parallel";
+    }
+
+    Cam(double x, double y, double z, double rx, double ry, double rz, int sxin, int syin): trans(4, 4) {
+        trans = linalg::get_homo_trans(x, y, z, rx, ry, rz);
+        sx = sxin;
+        sy = syin;
+        mode = "parallel";
+    }
+
+    sf::VertexArray project(Tri_3d &in) {
+        /*
+        vector<linalg::Matrix> ps;
+        ps.emplace_back(vector<vector<double>> ({{static_cast<double>(in.P1().getp()[0])}, {static_cast<double>(in.P1().getp()[1])}}));
+        ps.emplace_back(vector<vector<double>> ({{static_cast<double>(in.P2().getp()[0])}, {static_cast<double>(in.P2().getp()[1])}}));
+        ps.emplace_back(vector<vector<double>> ({{static_cast<double>(in.P3().getp()[0])}, {static_cast<double>(in.P3().getp()[1])}}));
+*/
+        sf::VertexArray out(sf::PrimitiveType::Triangles, 3);
+        for (int i = 0; i < 3; i++) {
+            auto temp = (trans*in.at(i)).getp();
+            out[i].position = sf::Vector2f(static_cast<float>(temp[0]), static_cast<float>(temp[1]));
+        }
+        return out;
     }
 };
 using namespace linalg;
@@ -157,12 +245,16 @@ int main() {
     vector<Tri_3d> trs;
 
     //manueller input des Würfels
-    trs.emplace_back(Point(0, 0, 0), Point(0, 0, 1), Point(0, 0, 1));
+    trs.emplace_back(Point(0, 0, 0), Point(0, 0, 1), Point(0, 1, 0));
     trs.emplace_back(Point(0, 0, 1), Point(0, 1, 1), Point(0, 1, 0));
     trs.emplace_back(Point(0, 0, 1), Point(0, 1, 1), Point(1, 1, 1));
-    trs.emplace_back(Point(0, 0, 0), Point(0, 0, 1), Point(0, 0, 1));
+    trs.emplace_back(Point(0, 0, 1), Point(1, 0, 1), Point(1, 1, 1));
+    trs.emplace_back(Point(0, 1, 0), Point(0, 1, 1), Point(1, 1, 1));
 
-
+    //Cam tests
+    Cam c{};
+    auto t = c.project(trs[0]);
+    cout << "pooper" << endl;
     /*
     sf::RenderWindow win(sf::VideoMode({800, 600}), "pooper");
     win.setFramerateLimit(60);
